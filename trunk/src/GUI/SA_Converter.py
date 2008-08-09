6531f6997261cdc6bb8019418017469433bfe677
laszlo.mates <laszlo.mates@732492aa-5b49-0410-a19c-07a6d82ec771>
2008-08-09 15:19:02 +0000

    SA Converter

---
+++ trunk/src/GUI/SA_Converter.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+#---------------------------------------------------------------------------------------------------------------------
+#
+#    Swiss Army  conversion script....
+#
+#    Created by: Laszlo Mates (laszlo.mates@gmail.com)
+#    2008
+#
+#---------------------------------------------------------------------------------------------------------------------
+
+import sys
+import os
+
+starterdir=os.getcwd()
+dircontent=os.listdir(starterdir)
+
+#print os.path.isfile()
+
+print dircontent
